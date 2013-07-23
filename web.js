c72231a33a81600540fb013aa724532a37d00103
Ubuntu <ubuntu@ip-172-31-29-211.us-west-2.compute.internal>
2013-07-23 06:34:56 +0000

    styled page

--- web.js
+++ web.js
@@ -5,13 +5,11 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 
 var data = fs.readFileSync('index.html', 'utf8');
-  console.log(data);
- response.send(data);
-
+  response.send(data);
   response.send('Hello World2!');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
